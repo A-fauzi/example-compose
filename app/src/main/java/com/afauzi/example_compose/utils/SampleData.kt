bceefcc87756212115910a3b07c911bb80a918eb
Akhmad Fauzi <akhmadfauzi9797@gmail.com>
2022-09-07 21:17:50 +0700

    Create List with lazyColumn

--- app/src/main/java/com/afauzi/example_compose/utils/SampleData.kt
+++ app/src/main/java/com/afauzi/example_compose/utils/SampleData.kt
@@ -3,7 +3,7 @@ package com.afauzi.example_compose.utils
 import com.afauzi.example_compose.model.Message
 
 object SampleData {
-    private val message1 = Message("akhmad", "Hello")
-    private val message2 = Message("fauzi", "Hello yaaaaaaa")
+    private val message1 = Message("akhmad", "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ")
+    private val message2 = Message("fauzi", "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, ")
     val conversationSample = arrayListOf(message1, message2)
 }
