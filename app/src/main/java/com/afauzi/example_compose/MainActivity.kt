7492929dc636093c8c2c4fa598e7e57d85efbdac
Akhmad Fauzi <akhmadfauzi9797@gmail.com>
2022-09-07 19:58:54 +0700

    Use material design in compose

--- app/src/main/java/com/afauzi/example_compose/MainActivity.kt
+++ app/src/main/java/com/afauzi/example_compose/MainActivity.kt
@@ -21,7 +21,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(Message("Akhmad Fauzi", "Luar Biasa"))
+            MaterialTheme() {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MessageCard(Message("Akhmad Fauzi", "Luar Biasa"))
+                }
+            }
         }
     }
 
@@ -54,6 +58,10 @@ class MainActivity : ComponentActivity() {
     @Preview
     @Composable
     fun PreviewMessageCard() {
-        MessageCard(Message("Akhmad Fauzi", "Luar Biasa"))
+        MaterialTheme {
+            Surface(modifier = Modifier.fillMaxSize()) {
+                MessageCard(Message("Akhmad Fauzi", "Luar Biasa"))
+            }
+        }
     }
 }
