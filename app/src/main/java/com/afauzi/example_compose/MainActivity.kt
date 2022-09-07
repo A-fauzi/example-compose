6cf54010136bc24e38173c8bb653397f205496d5
Akhmad Fauzi <akhmadfauzi9797@gmail.com>
2022-09-07 19:50:39 +0700

    Add Image Element and configure in layout

--- app/src/main/java/com/afauzi/example_compose/MainActivity.kt
+++ app/src/main/java/com/afauzi/example_compose/MainActivity.kt
@@ -3,14 +3,18 @@ package com.afauzi.example_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.afauzi.example_compose.ui.theme.ExamplecomposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -23,11 +27,33 @@ class MainActivity : ComponentActivity() {
 
     data class Message(val author: String, val body: String)
 
+
     @Composable
     fun MessageCard(msg: Message){
-        Column {
-            Text(msg.author)
-            Text(msg.body)
-        }
+       Row(
+           modifier = Modifier
+               .padding(15.dp)
+       ) {
+           Image(
+               painter = painterResource(id = R.drawable.ic_launcher_background),
+               contentDescription = "Example photo",
+               modifier = Modifier
+                   // Set Image Size
+                   .size(40.dp)
+                   // Clip image to be shaped as a circle
+                   .clip(CircleShape)
+           )
+           Spacer(modifier = Modifier.width(4.dp))
+           Column {
+               Text(msg.author)
+               Text(msg.body)
+           }
+       }
+    }
+
+    @Preview
+    @Composable
+    fun PreviewMessageCard() {
+        MessageCard(Message("Akhmad Fauzi", "Luar Biasa"))
     }
 }
